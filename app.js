93e25e472ec742353a22350fbed55a427d509bd3
Bhaskar Parihar <142592951+bhaskarparihar@users.noreply.github.com>
2026-05-04 09:39:37 +0530

    Set up server to listen on port 3000
    Added server listening on port 3000 with a console log.

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.put("/workouts/:id",(req, res)=>{
 app.delete("/workouts/:", (req,res)=>{
 
 })
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 
 
